ede6366a779327fe3978aa573242e52dd770f82b
seydouxxx <risc@kakao.com>
2021-11-29 18:52:49 +0900

    Initial commit

---
+++ douxChat/Controllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  douxChat
+//
+//  Created by Seydoux on 2021/11/29.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
